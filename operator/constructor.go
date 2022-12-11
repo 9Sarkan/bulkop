3b1b82e2226d65d0ffa567f6dab61940d0c27dfa
sarkan <sarkan.mohammady@snapp.cab>
2022-12-11 23:43:04 +0330

    pipe constructor modified

--- operator/constructor.go
+++ operator/constructor.go
@@ -2,19 +2,24 @@ package operator
 
 import "time"
 
+func NewLine() Line {
+	return Line{}
+}
+
 // NewPipe create a pipe to handle request chan objects and put result on response channel
-func NewPipe(name string, requestChan, responseChan chan interface{}, processor Processor,
-	worker int, errorHandler ErrorHandler, processorTimeout time.Duration) {
+func NewPipe(name string, requestChan chan interface{}, processor Processor,
+	worker int, errorHandler ErrorHandler, processorTimeout time.Duration) Pipe {
 	pipe := Pipe{
 		name:             name,
 		processor:        processor,
 		errorHandler:     errorHandler,
 		requestChan:      requestChan,
-		responseChan:     responseChan,
+		responseChan:     make(chan interface{}),
 		centralKiller:    make(chan struct{}),
 		processorTimeout: processorTimeout,
 	}
 	for i := 0; i < worker; i++ {
 		pipe.NewWorker()
 	}
+	return pipe
 }
